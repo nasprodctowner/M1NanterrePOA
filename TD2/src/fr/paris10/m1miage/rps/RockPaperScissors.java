c91b6c7672f595fc718834ffdaf601c714d2ea33
Nassim KHATIR <khatirnassim@gmail.com>
2017-10-12 09:10:36 +0200

    cycle de tdd

--- TD2/src/fr/paris10/m1miage/rps/RockPaperScissors.java
+++ TD2/src/fr/paris10/m1miage/rps/RockPaperScissors.java
@@ -3,6 +3,31 @@ package fr.paris10.m1miage.rps;
 public class RockPaperScissors {
 
     Result play(RPSEnum p1, RPSEnum p2) {
-        return Result.WIN;
+
+       if(p1 == p1.SCISSORS){
+           if (p2 == p2.ROCK){
+               return Result.LOST;
+           }
+           if (p2 == p2.PAPER){
+               return Result.WIN;
+           }
+       }
+       else if(p1 == p1.ROCK){
+           if (p2 == p2.SCISSORS){
+               return Result.WIN;
+           }
+           if (p2 == p2.PAPER){
+               return Result.LOST;
+           }
+       }
+       else if(p1 == p1.PAPER){
+           if (p2 == p2.SCISSORS){
+               return Result.LOST;
+           }
+           if (p2 == p2.ROCK){
+               return Result.WIN;
+           }
+       }
+       return Result.TIE;
     }
 }
--- TD2/test/fr/paris10/m1miage/rps/RockPaperScissorsTest.java
+++ TD2/test/fr/paris10/m1miage/rps/RockPaperScissorsTest.java
@@ -9,7 +9,9 @@ import static org.testng.Assert.*;
 
 public class RockPaperScissorsTest {
 
+
     RockPaperScissors rps;
+
     @BeforeMethod
     public void setUp() throws Exception {
         rps = new RockPaperScissors();
@@ -25,5 +27,18 @@ public class RockPaperScissorsTest {
     public void testWinPlay(String p1, String p2) throws Exception {
         assertEquals(rps.play(RPSEnum.valueOf(p1),RPSEnum.valueOf(p2)),Result.WIN);
     }
+
+    @Parameters({"paper","paper"})
+    @Test
+    public void testTiePLay(String p1, String p2){
+        assertEquals(rps.play(RPSEnum.valueOf(p1),RPSEnum.valueOf(p2)),Result.TIE);
+    }
+
+    @Parameters({"paper","scissors"})
+    @Test
+    public void testLostPlay(String p1, String p2){
+        assertEquals(rps.play(RPSEnum.valueOf(p1),RPSEnum.valueOf(p2)),Result.LOST);
+    }
+
 
 }
